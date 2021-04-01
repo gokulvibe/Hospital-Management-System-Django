4acdaed1f3f5f15e7a1e920f5fbc2c69cdd05c35
Gokul Raam <gokulraamofficial@gmail.com>
2021-04-01 15:58:31 +0530

    Created new app for main functionalities and created models

---
+++ age_of_heroes/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AgeOfHeroesConfig(AppConfig):
+    name = 'age_of_heroes'
---
+++ age_of_heroes/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+class Appointment(models.Model):
+    doctor = models.ForeignKey(User, related_name='doctor', on_delete=models.CASCADE)
+    patient = models.ForeignKey(User, related_name='patient', on_delete=models.CASCADE)
+    time_slot = models.DateTimeField()
+    status = models.CharField()
+    
+class MedicalReport(models.Model):
+    patient = models.ForeignKey(User, related_name='patient', on_delete=models.CASCADE)
+    blood_sugar_level = models.FloatField()
+    blood_pressure = models.FloatField()
+    body_temperature = models.FloatField()
+    weight = models.FloatField()
+    xray = models.BooleanField()
+    mri_scan = models.BooleanField()
+    blood_sodium_level = models.FloatField()
+    other_diagonosis = models.TextField()
+    cause = models.CharField()
+    height = models.FloatField()
